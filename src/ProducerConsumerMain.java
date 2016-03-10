2797a2238fbf62945b175ea59054bf6a64a5c825
bhavintandel <bhautandel@yahoo.com.com>
2016-03-10 15:56:42 +0000

    Added multi producer and consumer

--- src/ProducerConsumerMain.java
+++ src/ProducerConsumerMain.java
@@ -4,9 +4,18 @@ public class ProducerConsumerMain {
 		Box box = new Box();
 		Thread p = (new Thread(new Producer(box)));
 		Thread c = (new Thread(new Consumer(box)));
+		Thread p1 = (new Thread(new Producer(box)));
+		Thread c1 = (new Thread(new Consumer(box)));
+		Thread p2 = (new Thread(new Producer(box)));
+		Thread c2 = (new Thread(new Consumer(box)));
 		
 		p.start();
 		c.start();
+		p1.start();
+		c1.start();
+		p2.start();
+		c2.start();
+		
 		
 	}
 	
