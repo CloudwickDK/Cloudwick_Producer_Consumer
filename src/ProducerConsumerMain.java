4e66a02cc1eb8d6a695b710af757c42128e76270
bhavintandel <bhautandel@yahoo.com.com>
2016-03-10 16:08:22 +0000

    Commented some thread

--- src/ProducerConsumerMain.java
+++ src/ProducerConsumerMain.java
@@ -6,15 +6,15 @@ public class ProducerConsumerMain {
 		Thread c = (new Thread(new Consumer(box)));
 		Thread p1 = (new Thread(new Producer(box)));
 		Thread c1 = (new Thread(new Consumer(box)));
-		Thread p2 = (new Thread(new Producer(box)));
-		Thread c2 = (new Thread(new Consumer(box)));
+		//Thread p2 = (new Thread(new Producer(box)));
+		//Thread c2 = (new Thread(new Consumer(box)));
 		
 		p.start();
 		c.start();
 		p1.start();
 		c1.start();
-		p2.start();
-		c2.start();
+		//p2.start();
+		//c2.start();
 		
 		
 	}
