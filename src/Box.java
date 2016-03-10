8528fc1f0abf35718046b09329471d9ea8085156
bhavintandel <bhautandel@yahoo.com.com>
2016-03-10 15:54:40 +0000

    Implemented queue support

--- src/Box.java
+++ src/Box.java
@@ -6,41 +6,43 @@ import java.util.LinkedList;
  * @since  10/03/2016; 12:41
  */
 public class Box {
-	private LinkedList<String> message ;
-	private boolean empty;
+	private LinkedList<String> messages ;
+	private int queueSize;
+	public boolean empty;
 	
 	public Box(){
+		messages = new LinkedList<String>();
+		queueSize = 5;
 		empty = true;
 	}
 	
 	public synchronized String take(){
 		while(empty){
 			try{
-				//System.out.println("I am in take function");
+				System.out.println("I am in take function");
 				wait();
 			}catch (InterruptedException e) {
 				//System.out.println("I am in take function");
 			}
 		}
 		notifyAll();
-		empty = message.isEmpty();
-		return message.pop();
+		String message= messages.pop();
+		empty = messages.isEmpty();
+		return message;
 	}
 	
 	public synchronized void put(String message){
-		while(!empty){
+		while(this.messages.size() >= queueSize){
 			try{
-				//System.out.println("I am in put function");
-
+				System.out.println("I am in put function");
 				wait();
 			} catch (InterruptedException e) {
 				//System.out.println("I am in take function");
 			}
 		}
+		this.messages.push(message);
 		empty = message.isEmpty();
-		this.message.push(message);
 		notifyAll();
-
 	}
 
 }
